c0000c0c4ecee50a5abdca8f92d601843e0c6815
leonTech254 <martinleontech23@gmail.com>
2024-02-20 18:12:06 +0300

    --fixed Multple Cors

--- src/main/java/com/cysecinnovation/cysecinnovations_gateway/Config/WebFilterConfig.java
+++ src/main/java/com/cysecinnovation/cysecinnovations_gateway/Config/WebFilterConfig.java
@@ -7,9 +7,28 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.server.WebFilter;
 import reactor.core.publisher.Mono;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class WebFilterConfig {
+    @Bean
+    public WebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("Content-Type", "Accept", "Authorization"));
+        config.setExposedHeaders(List.of("Access-Control-Allow-Methods", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin", "Access-Control-Max-Age", "Content-Length", "X-Application-Context"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+
     @Bean
     public WebFilter optionsFilter() {
         return (exchange, chain) -> {
